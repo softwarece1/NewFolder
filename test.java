80e3f39867543c65e72354d5eeb7d94cccb175c8
Hello <world@bcc.com>
2021-03-22 09:51:38 +0530

    hw

---
+++ test.java
@@ -0,0 +1,10 @@
+class test {
+	public static void main(String[] args) {
+		List<Integer> ls1 = new ArrayList<Integer> ();
+		List<Integer> ls2 = l1;
+		ls1.add(1);
+		
+		System.out.println(ls1);
+		System.out.println(ls2);
+	}
+}
